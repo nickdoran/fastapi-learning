03c4cb1f1a088fa1b918bc2d52d63198228b086f
Nicklas Doran <nickdoran6@gmail.com>
2025-10-28 22:06:05 -0400

    init: added imports, mock users, and some example endpoints

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, HTTPException, status, Path
+from typing import Optional
+from pydantic import BaseModel
+
+app = FastAPI()
+
+users = {
+    1 : {
+        "name":"Josh",
+        "website": "www.apple.com",
+        "age": 28,
+        "role": "developer"
+    }
+}
+
+@app.get("/")
+def root():
+    return {"Message": "Hello World"}
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int = Path(..., description="The ID you want to get", gt=0, lt=100)):
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail="User Not Found!")
+    return users[user_id];
+    
