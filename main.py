2e5ac09f61ba11050f08de8375cc90a8d422919c
Nicklas Doran <nickdoran6@gmail.com>
2025-10-28 22:56:24 -0400

    feat: added a post method to create a user

--- main.py
+++ main.py
@@ -38,3 +38,26 @@ def get_user(user_id: int = Path(..., description="The ID you want to get", gt=0
         raise HTTPException(status_code=404, detail="User Not Found!")
     return users[user_id];
     
+@app.post("/users/{user_id}", status_code=status.HTTP_201_CREATED)
+def create_user(user_id: int, user:User):
+    if user_id in users:
+        raise HTTPException(status_code=404, detail="User already exists!")
+    users[user_id] = user.model_dump()
+    return user
+
+
+@app.put("/users/{user_id}")
+def update_user(user_id: int, user: UpdateUser):
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail="User is not here")
+    
+    current_user = users[user_id]
+
+    if user.name is not None:
+        current_user["name"] = user.name
+    if user.website is not None:
+        current_user["website"] = user.website
+    if user.age is not None:
+        current_user["age"] = user.age
+    
+    
